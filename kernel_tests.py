f44ac9aa83e85ffd7e4e92673c8ff88bc744648f
lorenzovaccarini <lorenzomaria.vaccarini@mail.polimi.it>
2024-12-16 16:06:04 +0100

    Develop plugin and planner use cases

--- kernel_tests.py
+++ kernel_tests.py
@@ -1,13 +1,14 @@
 from semantic_kernel import Kernel
 from semantic_kernel.connectors.ai.open_ai import AzureChatPromptExecutionSettings, AzureChatCompletion
 from semantic_kernel.contents import ChatHistory, ChatMessageContent, AuthorRole, ImageContent
-
+from semantic_kernel.planners import SequentialPlanner
 
 class KernelTests:
     def __init__(self):
         self.kernel = Kernel()
         self.execution_settings = AzureChatPromptExecutionSettings()
         self.chat_completion_id = None
+        self.plugin_dir = "./plugins"
 
     def link_azureoai_service(self, service_id, env_path):
         azure_chat_completion = AzureChatCompletion(
@@ -53,5 +54,21 @@ class KernelTests:
             settings=self.execution_settings
         )
         print(model_response)
+
+    async def execute_function_test(self, plugin_name, num, formatting):
+        sample_plugins = self.kernel.add_plugin(parent_directory=self.plugin_dir, plugin_name=plugin_name)
+        random_function = sample_plugins["random_numbers"]
+        result = await self.kernel.invoke(random_function, num=num, formatting=formatting)
+        print(result)
+
+    async def execute_function_planner_test(self, service_id, query):
+        planner = SequentialPlanner(self.kernel, service_id)
+        sequential_plan = await planner.create_plan(goal=query)
+        print("The plan's steps are:")
+        for step in sequential_plan._steps:
+            print(
+                f"- {step.description.replace('.', '') if step.description else 'No description'} using {step.metadata.fully_qualified_name} with parameters: {step.parameters}"
+            )
+
 
 
--- main.py
+++ main.py
@@ -5,9 +5,14 @@ from kernel_tests import KernelTests
 
 async def main():
     kernel_tests = KernelTests()
-    kernel_tests.link_azureoai_service("azureoai_test_id", ".env")
-    await kernel_tests.execute_simplest_test()
-    await kernel_tests.execute_image_content_test()
+    service_id = "azureoai_test_id"
+    kernel_tests.link_azureoai_service(service_id, ".env")
+    # await kernel_tests.execute_simplest_test()
+    # await kernel_tests.execute_image_content_test()
+
+    # How can I invoke this function semantically??
+    await kernel_tests.execute_function_test("sample_plugin", "10", "{num_0; num_1; ... num_n}")
+    await kernel_tests.execute_function_planner_test(service_id, "How can I generate 100 random numbers? The formatting should be as such: '[num_0, num_1, ..., num_n]")
 # Run the main function
 if __name__ == "__main__":
     asyncio.run(main())
